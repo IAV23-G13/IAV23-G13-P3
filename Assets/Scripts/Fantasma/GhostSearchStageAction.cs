3db25e65c8a5fee99f9984c988a326fe1a0ef084
ivo_hr <81875802+ivo-hr@users.noreply.github.com>
2023-04-16 18:39:46 +0200

    IT WOOORKS

--- Assets/Scripts/Fantasma/GhostSearchStageAction.cs
+++ Assets/Scripts/Fantasma/GhostSearchStageAction.cs
@@ -13,6 +13,7 @@ using UnityEngine;
 using BehaviorDesigner.Runtime.Tasks;
 using UnityEngine.AI;
 using BehaviorDesigner.Runtime;
+using UnityEditor.Experimental.GraphView;
 
 /*
  * Accion de ir al escenario, cuando llega devuelve Success
@@ -22,27 +23,54 @@ public class GhostSearchStageAction : Action
 {
     NavMeshAgent agent;
     Transform stage;
-
+    GameBlackboard GameBlackboard;
+    GameObject target;
+    bool bothdown = false;
     public override void OnAwake()
     {
         // IMPLEMENTAR 
         agent = GetComponent<NavMeshAgent>();
         stage = (Owner.GetVariable("escenario") as SharedTransform).Value;
+        GameBlackboard = GameObject.FindGameObjectWithTag("Blackboard").GetComponent<GameBlackboard>();
+        target = GameBlackboard.nearestLever(gameObject);
+        agent.destination = target.transform.position;
+    }
+
+    private bool AreBothDown()
+    {
+        bool areThey = GameBlackboard.nearestLever(gameObject) == null;
+        if (!areThey)
+        {
+            //Todo menos el vestibulo
+            agent.areaMask = (1 << 0) | (1 << 1) | (1 << 2) | (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7) | (1 << 8) | (1 << 9) | (1 << 10) | (1 << 11) | (1 << 12) |
+                (1 << 13) | (1 << 14);
+        }
+
+        else agent.areaMask = -1;
+        return areThey;
     }
 
     public override TaskStatus OnUpdate()
     {
         // 
-        agent.SetDestination(stage.position);
+        //
 
-        if (Vector3.Distance(transform.position, stage.position) < 2f)
+
+        if (Vector3.Distance(transform.position, target.transform.position) < 2f && !AreBothDown())
         {
-            stage.GetComponent<Cantante>().capturadaPor(this.transform);
+            target.GetComponentInChildren<ControlPalanca>().caido = true;
+            if (!AreBothDown())
+            {
+                target = GameBlackboard.nearestLever(gameObject);
+                return TaskStatus.Running;
+            }
             return TaskStatus.Success;
         }
+        else if (AreBothDown()) return TaskStatus.Success;
         else
         {
-            return TaskStatus.Failure;
+            agent.destination = target.transform.position;
+            return TaskStatus.Running;
         }
     }
 }
